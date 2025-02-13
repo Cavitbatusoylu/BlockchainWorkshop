a7a09edf7f3ec923c02fabdbc384599e61ef2feb
Cavitbatusoylu <CECBSofficial@gmail.com>
2025-02-13 05:02:07 +0300

    colorama

--- _01_AES.py
+++ _01_AES.py
@@ -23,8 +23,7 @@ VPN
 from Crypto.Cipher import AES  # AES ŞİFRELEME KÜTÜPHANESİ
 from Crypto.Util.Padding import pad, unpad # Veri bloklarını tamamlamak ve kaldırma fonksiyonu
 import os # Rastgele anahatar üretmek için kullanılan küütüphane
-
-from colorama import Fore, Style
+from colorama import Fore, Style #Renkli çıktı için
 
 ####################################################################
 #Renkleriçin kısayollar
