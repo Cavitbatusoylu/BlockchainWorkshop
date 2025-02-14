9a8ca91ed8964f13b21b59e07ac288d8a99da1a2
Cavitbatusoylu <CECBSofficial@gmail.com>
2025-02-14 04:56:49 +0300

    RSA

--- _03_RSA_ASİMETRİK.py
+++ _03_RSA_ASİMETRİK.py
@@ -1,83 +1,101 @@
-# pip --vesion
-# pip install pycrptodome  # AES KÜTÜPHANESİ
-#pip install colorama #Color
+# pip --version
+# pip install pycryptodo    # AES kütüphanesi
+# pip install colorama      # Color
 
 # Eğer paket yüklenmezse
-# pip uninstall pycrptodome
-# pip install pycryptodome --no--cache-dir
+# pip uninstall pycryptodo
+# pip install pycryptodo --no-cache-dir
 
-#CBC(Cipher Block Chaining)
-#Simetrik: Aynı anahtar hem şifreleme hemde çözme işlemleri için kullanılır.
-#Asimetrik: İki farklı anahtar kullanılır(public, private)
+# BC (Cipher Block Chaining)
+# Simetrik: Aynı anahtar hem şifreleme hemde çözme işlemleri için kullanılır.
+# Asimetrik: İki farklı anahtar kullanılır(public,private)
 
-# 3.RSA (Rivest Shamir Adleman) - Asimetrik Şifreleme
-# 8 Bit = 1 Bayt
+# 3. RSA (Rivest Shamir Adleman) - Asimetrik Şifreleme
+# 8 bit= 1 Bayt
 
 """
-RSA (Rivest Shamir Adleman) - Asimetrik Şifreleme:
+AES,DES şifreleme algoritmalarında kulllanılır:
+Şifrelemede öncesinde PAD
+Şifre çözümlemede UNPAD
+
+pad, unpad bu fonksiyonlar AES,DES blok şifrelemede algoritmalarında
+veriyi istediğimizi formda kullanmak için bu fonksiyonunu çağırız.
+
+1-pad: Doldurma: veriyi belirli bir blok boyutuna tamamlamak içindir
+Sabit blok şifrelme algoritmalarıya çalışırız örneğin: 16 byte Eüer veriyi tam blok haline getirmezseni şifreleme çalışmaz.
+
+2-unpad: Kaldırma: şifrelenmiş verinin tamamına ulaşana kadar bırakılan paddingı kaldırır.
+pad tarafından eklenen fazlalıkları kaldırarak veriyi original haline getirmek
+"""
+
+"""
+RSA (Rivest Shamir Adleman) Asimetrik:
 1977 yılında geliştirilmiştir
-İki farklı anahatar kullanılır: Public, Private
-Anahtar uzunluğu: 1024-bit, 2048-bit, 4096-bit kullanılır ancak biz genelde 2048-bit kullanırız.
-AES, DES'e göre çok daha yavaştır.
+İki farklı anahtar (public,private) kullanılır.
+Anahtar uzunluğu: 1024-bit, 2048-bit, 4096-bit kullanılır ancak biz genelde 2048-bit kullanılır.
+AES,DES'e göre çok daha yavaştır.
 
-SSL/TLS(HTTPS)
-Digital İmza
-Kripto Para cüzdanlarında
+SSL/TLS (HTTPS)
+Digital imza
+Kripto para cüzdanlarında
 
-Anahtar Üretme: Büyük asal sayılar kullanılır. 2,3,5,7,11,13,17,19,23 ....
+Anahtar üretim: Büyük asal sayılar kullanılır. 2,3,5,7,11,13,17,19,23 ...
 public key: Veriyi şifrelemek
 private key: Veriyi çözmek için gereken anahtar
 """
 
-####################################################################
-from Crypto.PublicKey import RSA # RSA ŞİFRELEME KÜTÜPHANESİ
-from Crypto.Cipher import PKCS1_OAEP # RSA Anahtar Üretme Kütüphanesi
-#import os # Rastgele anahatar üretmek için kullanılan kütüphane
-from colorama import Fore, Style #Renkli çıktı için
+########################################################################
+from Crypto.PublicKey import RSA         # RSA şifreleme kütüphanesi
+from Crypto.Cipher import PKCS1_OAEP    #
+# import os # Rastgelere anahtar üretmek için kullanılan kütüphanesi
+from colorama import Fore, Style # Renkli çıktı için
 
-####################################################################
-#Renkleriçin kısayollar
-RED = Fore.RED
-GREEN = Fore.GREEN
-BLUE = Fore.BLUE
-YELLOW = Fore.YELLOW
-MAGENTA = Fore.MAGENTA
-CYAN = Fore.CYAN
-WHITE = Fore.WHITE
-RESET = Style.RESET_ALL #Renkleri sıfırlamak için kullanılır
-####################################################################
+########################################################################
+# Renkler için kısayollar
+RED= Fore.RED
+GREEN= Fore.GREEN
+YELLOW= Fore.YELLOW
+BLUE= Fore.BLUE
+MAGENTA= Fore.MAGENTA
+CYAN= Fore.CYAN
+WHITE= Fore.WHITE
+RESET=Style.RESET_ALL # Renk sıfırlamak
 
-# 8 bit = 1 byte
-# RSA Anahtar üretmek
-#key = os.urandom(8) # 256-bit DES anahtarı güvenli ve rastgele sayılar için(2.Yol) AES
-#key = b"8bytekey1" # 8 byte uzunluğunda bir anahtar (1.Yol) DES
-key = RSA.generate(2048) # 2048-bit uzunluğunda bir RSA anahtarı üret
+########################################################################
+#  8 bit = 1 byte
+# RSA Anahtar ürütmek
+# key = os.urandom(8) # 256-bit DES anahtarı güvenli ve rastgele için (2.YOL) AES
+# key = b"8bytkey1" # 8 bayt uzunluğunda bir anahtar (1.YOL) DES
+key = RSA.generate(2048) # 2048-bit RSA anahtarı üret
 public_key = key.publickey().export_key()
 private_key = key.export_key()
 
-print(f"{YELLOW}Genel Anahtar (RSA): ", public_key.decode()+"\n")
-print(f"{BLUE}Özel Anahtar (RSA): ", private_key.decode()+"\n")
+print(f"{YELLOW}Genel Anahtar (RSA): ", public_key.decode()+"\n\n")
+print(f"{RED}Özel Anahtar (RSA): ", private_key.decode()+"\n\n")
 
-#Şifrelenecek veriyi tamamladık(Ör: Cavit Batu Soylu)
-data =  "Cavit Batu Soylu" .encode() # string veriyi byte formatına çevirir
 
-rsa_public_key = RSA.import_key(public_key)
+# Şifrelenecek veriyi tamamladık (Örnek: e-posta adresi)
+data = "Cavit Batu Soylu".encode() # string veriyi byte formanıta çevir
 
-# Şifreleme (Public Key kullanarak)
-cipher = PKCS1_OAEP.new(rsa_public_key) # RSA nesnesini oluştur
+#
+rsa_public_key =RSA.import_key(public_key)
 
-# Veriyi DES şifreleme bloğu boyutuna uygun hale getirmek için pad() kullanıyoruz.
-# DES boyutu 8 byte olduğu için eksik kalan kısımları uygun bir şemada dolduralım.
-ciphertext = cipher.encrypt(data)
+# Şifreleme (Public Key)
+cipher = PKCS1_OAEP.new(rsa_public_key) # DES nesnesini oluştur (CBC modu ve IV)
 
-#Şifrelenmiş verileri hexadecimal formatında ekrana yazdırıyoruz.
-# print(f"{CYAN}Şifrelenmiş Veri (RSA):  {RESET} {ciphertext.hex()}" )
-print(f"{CYAN}Şifrelenmiş Veri (RSA):  {RESET}" , ciphertext.hex()+"\n")
 
-# RSA şifre çözme (private key kullanarak)
+# Veriyi AES şifreleme bloğu boyutuna uygun hale getirmek için pad() kullanılıyoruz.
+# AES blok boyutu 16 byte olduğu için eksik kalan kısımları uygun bir şemada dolduralım.
+ciphertext= cipher.encrypt(data)
+
+# Şifrelenmiş verileri hexadecimal formatında ekrana yazdırıyoruz.
+# print(f"{BLUE}Şifrelenmiş Veri (AES, CBC Modu): {RESET}{ciphertext.hex()} ")
+print(f"{CYAN}Şifrelenmiş Veri (RSA): {RESET} ", ciphertext.hex()+"\n")
+
+# RSA şifre çözme (private key)
 rsa_private_key = RSA.import_key(private_key)
-decipher = PKCS1_OAEP.new(rsa_private_key)
-decrpyted_data = decipher.decrypt(ciphertext)
+decipher= PKCS1_OAEP.new(rsa_private_key)
+decrypted_data= decipher.decrypt(ciphertext)
 
-# Şifre çözülmüş veriyi ekrana yazdırıyoruz.
-print(f"{CYAN}Çözülmüş İlk Veri: (DES): {RESET}" , decrpyted_data.decode())
+# Şifre çözülmüş verileri ekrana yazdırıyoruz.
+print(f"{CYAN}Çözülmüş ilk veri: (RSA): {RESET} ", decrypted_data.decode())
